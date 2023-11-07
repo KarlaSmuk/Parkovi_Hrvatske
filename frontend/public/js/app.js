4df44e131604d08ac116323d3f246a9efa532b0e
KarlaSmuk <karla.smuk@gmail.com>
2023-11-07 09:18:42 +0100

    Fixed UI

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,16 +1,3 @@
-
-
-
-function fetchByName() {
-    fetch("http://localhost:8080/park/Mljet")
-        .then(res => {
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-        });
-}
-
 function fetchAll() {
 
     fetch("http://localhost:8080/parks")
@@ -25,127 +12,6 @@ function fetchAll() {
         });
 
 }
-
-// function fillTableWithData(data) {
-
-
-//     const tableBody = document.querySelector(".table > tbody");
-
-//     data.forEach((park) => {
-//         park.animals.forEach((animal) => {
-
-//             if (park.counties.length > 1) {
-
-//                 park.counties.forEach((county) => {
-//                     const trElement = document.createElement("tr");
-
-//                     const parkNameCell = document.createElement("td");
-//                     parkNameCell.innerText = park.parkName;
-
-//                     const typeOfParkCell = document.createElement("td");
-//                     typeOfParkCell.innerText = park.typeOfPark;
-
-//                     const yearOfFoundationCell = document.createElement("td");
-//                     yearOfFoundationCell.innerText = park.yearOfFoundation;
-
-//                     const areaCell = document.createElement("td");
-//                     areaCell.innerText = park.area;
-
-//                     const peakNameCell = document.createElement("td");
-//                     peakNameCell.innerText = park.peakName;
-
-//                     const peakHeightCell = document.createElement("td");
-//                     peakHeightCell.innerText = park.peakHeight;
-
-//                     const countiesCell = document.createElement("td");
-//                     countiesCell.innerText = county;
-
-//                     const atractionCell = document.createElement("td");
-//                     atractionCell.innerText = park.atraction;
-
-//                     const eventCell = document.createElement("td");
-//                     eventCell.innerText = park.event;
-
-//                     const animalNameCell = document.createElement("td");
-//                     animalNameCell.innerText = animal.name;
-
-//                     const animalSpeciesCell = document.createElement("td");
-//                     animalSpeciesCell.innerText = animal.species;
-
-//                     trElement.appendChild(parkNameCell);
-//                     trElement.appendChild(typeOfParkCell);
-//                     trElement.appendChild(yearOfFoundationCell);
-//                     trElement.appendChild(areaCell);
-//                     trElement.appendChild(peakNameCell);
-//                     trElement.appendChild(peakHeightCell);
-//                     trElement.appendChild(countiesCell);
-//                     trElement.appendChild(atractionCell);
-//                     trElement.appendChild(eventCell);
-//                     trElement.appendChild(animalNameCell);
-//                     trElement.appendChild(animalSpeciesCell);
-
-//                     tableBody.appendChild(trElement);
-//                 })
-
-//             } else {
-//                 const trElement = document.createElement("tr");
-
-//                 const parkNameCell = document.createElement("td");
-//                 parkNameCell.innerText = park.parkName;
-
-//                 const typeOfParkCell = document.createElement("td");
-//                 typeOfParkCell.innerText = park.typeOfPark;
-
-//                 const yearOfFoundationCell = document.createElement("td");
-//                 yearOfFoundationCell.innerText = park.yearOfFoundation;
-
-//                 const areaCell = document.createElement("td");
-//                 areaCell.innerText = park.area;
-
-//                 const peakNameCell = document.createElement("td");
-//                 peakNameCell.innerText = park.peakName;
-
-//                 const peakHeightCell = document.createElement("td");
-//                 peakHeightCell.innerText = park.peakHeight;
-
-//                 const countiesCell = document.createElement("td");
-//                 countiesCell.innerText = park.counties; // Join counties as a comma-separated string
-
-//                 const atractionCell = document.createElement("td");
-//                 atractionCell.innerText = park.atraction;
-
-//                 const eventCell = document.createElement("td");
-//                 eventCell.innerText = park.event;
-
-//                 const animalNameCell = document.createElement("td");
-//                 animalNameCell.innerText = animal.name;
-
-//                 const animalSpeciesCell = document.createElement("td");
-//                 animalSpeciesCell.innerText = animal.species;
-
-//                 trElement.appendChild(parkNameCell);
-//                 trElement.appendChild(typeOfParkCell);
-//                 trElement.appendChild(yearOfFoundationCell);
-//                 trElement.appendChild(areaCell);
-//                 trElement.appendChild(peakNameCell);
-//                 trElement.appendChild(peakHeightCell);
-//                 trElement.appendChild(countiesCell);
-//                 trElement.appendChild(atractionCell);
-//                 trElement.appendChild(eventCell);
-//                 trElement.appendChild(animalNameCell);
-//                 trElement.appendChild(animalSpeciesCell);
-
-//                 tableBody.appendChild(trElement);
-//             }
-
-
-
-
-//         });
-//     });
-
-
-// }
 
 function fillTable(data) {
     const tableBody = document.querySelector(".table > tbody");
@@ -237,27 +103,11 @@ function clearTable() {
     }
 }
 
-// function fillDatatableNet() {
-//     new DataTable('#example', {
-//         "processing": true,
-//         "serverSide": true,
-//         "ajax": {
-//             "url": "http://localhost:8080/park",
-//             "dataSrc": ""
-//         },
+function downloadAsJSON() {
+    
+}
 
-//         columns: [
-//             { data: 'parkName' },
-//             { data: 'typeOfPark' },
-//             { data: 'yearOfFoundation' },
-//             { data: 'area' },
-//             { data: 'peakName' },
-//             { data: 'peakHeight' },
-//             { data: 'county' },
-//             { data: 'atraction' },
-//             { data: 'event' },
-//             { data: 'animal' },
-//             { data: 'species' }
-//         ]
-//     });
-// }
+function downloadAsCSV() {
+    
+}
+
