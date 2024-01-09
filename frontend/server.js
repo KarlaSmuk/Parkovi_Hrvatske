12689638449d1745b089e65e72bb3380fd36dede
Karla <karla.smuk@gmail.com>
2024-01-09 20:18:11 +0100

    Added first basic version of auth0

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const { join } = require('path');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER
+  };
+
 const app = express();
-
 app.use(express.static(join(__dirname, 'public')));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
-app.get("/", (req, res) => {
+    console.log(req.oidc.isAuthenticated());
     res.sendFile(join(__dirname, 'index.html'));
 });
 
@@ -16,7 +26,7 @@ app.get("/datatable", (req, res) => {
     res.sendFile(join(__dirname, 'datatable.html'));
 });
 
-app.listen(3000);
-
-
-module.exports = app;
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Express is running on port ${PORT}`)
+});
